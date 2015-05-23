44a5e508d03ce1ef07bb30d12f03f1d65c09664c
sundialmusic <paint@paintlocal.local>
2015-05-23 14:25:49 -0700

    Connect and build Contact Page

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var $ = jQuery;
+
+(function($) {
+
+function contactForm() {
+
+			$('#submit-form').click(function(){
+				var $this = $(this);
+				$('#contactform').submit(function(e) {
+					e.preventDefault();
+					$.post('php/contact.php', $('#contactform').serialize(), function(data){
+						$('.form-message').html(data).animate({opacity: 1}, 500, function(){
+							if ($(data).is('.send-true')){
+								$('#contactform').trigger( 'reset' );
+							}
+						});
+					});
+				});
+			});
+
+			//Under Construction (write to file)
+			$('.join-us').click(function(){
+				var $this = $(this);
+				$this.closest('.under-construction').submit(function(e) {
+					e.preventDefault();
+					$.ajax({
+						url  : 'php/u-c.php',
+						type : 'POST',
+						data : $this.closest('.under-construction').serialize(),
+						success : function(data){
+							$this.closest('.under-construction').find('.form-message').html(data).animate({opacity: 1}, 500, function(){
+								if ($(data).is('.send-true')){
+									$this.closest('.under-construction').trigger('reset');
+									$this.closest('.under-construction').find('.form-message').animate({opacity: 0}, 1000, function(){
+										$this.closest('.under-construction').find('.form-message').html('');
+									});
+								}
+							});
+						}
+					});
+				});
+			});
+			
+			if($(".mailchimp").length>0) {
+				$('.mailchimp').ajaxChimp({
+					url: '//animaltronix.us10.list-manage.com/subscribe/post?u=1282f8150c3ca30b5c76e4296&amp;id=75a4c59551'
+				});
+			}
+		}
+})(jQuery);
---
+++ php/contact.php
@@ -0,0 +1,28 @@
+<?php
+$to = 'gravitycoast-mailbot@animaltronix.com';
+$subject = 'You subject';
+$headers = 'From: (GravityCoast.com)' . "\r\n" . 'Content-type: text/html; charset=utf-8';
+$message = '
+<html>
+	<head>
+		<title>Contact Gravity Coast</title>
+	</head>
+	<body>
+		<h3>Name: <span style="font-weight: normal;">' . $_POST['name'] . '</span></h3>
+		<h3>Email: <span style="font-weight: normal;">' . $_POST['email'] . '</span></h3>
+		<div>
+			<h3 style="margin-bottom: 5px;">Comment:</h3>
+			<div>' . $_POST['comment'] . '</div>
+		</div>
+	</body>
+</html>';
+
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['comment'])) {
+	if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		mail($to, $subject, $message, $headers) or die('<span class="text-warning">Error sending Mail</span>');
+		echo '<span class="text-success send-true">Your email was sent!</span>';
+	}
+} else {
+	echo '<span class="text-danger">All fields must be filled!</span>';
+}
+?>
